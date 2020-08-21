45c55e8e008897f0d754dfccf7ac3c8bf390a230
Dominic Ferris <ferris.dominic@gmail.com>
2020-08-21 17:07:58 -0500

    create highscore controller+routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/high_scores/:game_title', to "high_scores#index"
+  post '/high_scores', to "high_scores#create"
 end
